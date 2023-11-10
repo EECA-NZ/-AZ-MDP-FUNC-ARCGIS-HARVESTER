9c95d870b12fe20a9dab6a32956ffd27555c932c
Bill Nelson <bill.nelson@eeca.govt.nz>
2023-11-11 00:41:37 +1300

    test: fix tests to work with refactored function

--- test/test_arcgis_harvester.py
+++ test/test_arcgis_harvester.py
@@ -1,33 +1,39 @@
-from unittest.mock import MagicMock, Mock, patch
+'''
+Tests for arcgis_harvester function
+'''
+
+from unittest.mock import patch
 
 import pytest
-from azure.storage.blob import BlobServiceClient
 
 from arcgis_harvester import (
-    fetch_metadata_from_layer,
-    fetch_token,
-    upload_to_blob,
-    write_to_csv,
+    __fetch_metadata_from_layer,
+    __fetch_token,
+    __upload_to_blob,
+    __write_to_csv,
 )
 
 
 def test_fetch_token_success():
+    '''Fetch token successfully'''
     with patch("requests.post") as mocked_post:
         mocked_post.return_value.status_code = 200
         mocked_post.return_value.json.return_value = {"token": "some-token"}
-        token = fetch_token()
+        token = __fetch_token()
         assert token == "some-token"
 
 
 def test_fetch_token_failure():
+    '''Fetch token unsuccessfully'''
     with patch("requests.post") as mocked_post:
         mocked_post.return_value.status_code = 200
         mocked_post.return_value.json.return_value = {"error": "some-error"}
-        token = fetch_token()
+        token = __fetch_token()
         assert token is None
 
 
 def test_fetch_metadata_from_layer_success():
+    '''Fetch metadata successfully'''
     token = "test-token"
     layer_url = "http://fake-url.com/layer"
     expected_metadata = {"name": "test-layer", "type": "Feature Layer"}
@@ -35,18 +41,19 @@ def test_fetch_metadata_from_layer_success():
     with patch("requests.get") as mocked_get:
         mocked_get.return_value.status_code = 200
         mocked_get.return_value.json.return_value = expected_metadata
-        metadata = fetch_metadata_from_layer(layer_url, token)
+        metadata = __fetch_metadata_from_layer(layer_url, token)
         assert metadata == expected_metadata
 
 
 def test_write_to_csv():
+    '''Write to csv successfully'''
     data = [
         {
             "properties": {"prop1": "value1", "prop2": "value2"},
             "geometry": {"type": "Point", "coordinates": [125.6, 10.1]},
         }
     ]
-    output = write_to_csv(data)
+    output = __write_to_csv(data)
     output.seek(0)
     lines = output.readlines()
     assert len(lines) > 1  # Header plus data line
@@ -56,12 +63,14 @@ def test_write_to_csv():
 
 @pytest.fixture
 def blob_service_client_mock():
+    '''Mock the blob service client'''
     # Mock for Azure BlobServiceClient
     with patch("azure.storage.blob.BlobServiceClient") as mock:
         yield mock
 
 
 def test_upload_to_blob(mocker):
+    '''Upload to blob successfully'''
     # Arrange
     connection_string = "DefaultEndpointsProtocol=https;AccountName=..."
     container_name = "my-container"
@@ -78,7 +87,7 @@ def test_upload_to_blob(mocker):
     )
 
     # Act
-    upload_to_blob(
+    __upload_to_blob(
         connection_string, container_name, blob_path_prefix, blob_name, blob_data
     )
 
@@ -90,8 +99,9 @@ def test_upload_to_blob(mocker):
 
     # Verify get_blob_client called with the correct container and blob path
     expected_blob_path = f"{blob_path_prefix}/{blob_name}"
-    mock_blob_service_client.from_connection_string.return_value.get_blob_client.assert_called_once_with(
-        container=container_name, blob=expected_blob_path
+    mock_blob_service_client.from_connection_string.return_value\
+        .get_blob_client.assert_called_once_with(
+            container=container_name, blob=expected_blob_path
     )
 
     # Verify blob_data.seek(0) was called to ensure the data is read from the beginning
